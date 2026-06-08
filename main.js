912e1b3549041c9ec5eca007dce9758ab042dc67
Patricia Ann Comendador <patriciaann.c10@gmail.com>
2026-06-08 19:32:18 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+// Esercizio 1
+// Restituisce true se un numero è pari, false se dispari.
+
+// const isPair = (num) => num % 2 === 0;
+// console.log(isPair(4));
+
+
+
+// Esercizio 2
+// Dati i valori di base e altezza, restituire l'area di un triangolo
+
+// const area = (base, altezza) => (base * altezza) / 2;
+// console.log(area(6, 4));
+
+
+
+// Esercizio 3
+// 
